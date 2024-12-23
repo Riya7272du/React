ae0b793fd8827a33784da1217e815b42231eac6d
riya7272 <riya.2022ca083@mnnit.ac.in>
2024-12-23 15:57:48 +0530

    files

---
+++ App.js
@@ -0,0 +1,10 @@
+const parent=React.createElement(
+    "div",
+    {id:"parent"},
+    React.createElement("h1",{id:"child"},
+        [React.createElement("h1",{},"I'm an H1 TAG"),
+        React.createElement("h2",{},"I'm an H2 TAG")]
+    )
+)
+const root=ReactDOM.createRoot(document.getElementById("root"));
+root.render(parent);
